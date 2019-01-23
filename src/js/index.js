b9b4ef273a62e47aa0842d3a715538e24cada3c2
anna <anna.lokietko@gmail.com>
2019-01-23 01:00:37 +0100

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 /* Here goes your JS code */
+ const buttonClickMe=document.querySelector("button");
+ const popupForm=document.querySelector(".popup");
+const buttonClikClose=document.querySelector(".close");
+
+buttonClickMe.addEventListener("click",()=>{
+popupForm.style.display="inline-block";
+});
+ 
+
+buttonClikClose.addEventListener("click",()=>{
+popupForm.style.display="none";
+});
