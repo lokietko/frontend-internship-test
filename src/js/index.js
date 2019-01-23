fa02f122366cd9c19aa1ea852d132420e26461a7
anna <anna.lokietko@gmail.com>
2019-01-23 14:06:49 +0100

    Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 /* Here goes your JS code */
  const buttonClickMe=document.querySelector("button");
  const popupForm=document.querySelector(".popup");
-const buttonClikClose=document.querySelector(".close");
+ const buttonClikClose=document.querySelector(".close");
+const submitButton=document.forms["form"]["submit"]
+const form=document.forms["form"];
 
 buttonClickMe.addEventListener("click",()=>{
 popupForm.style.display="inline-block";
@@ -11,3 +13,23 @@ popupForm.style.display="inline-block";
 buttonClikClose.addEventListener("click",()=>{
 popupForm.style.display="none";
 });
+
+
+form.addEventListener("change",function(){
+
+ const email=document.forms["form"]["email"].value;
+ const password=document.forms["form"]["password"].vaue;
+ const checkbox=document.forms["form"]["agreement"].checked;
+    if (email !== "" && email.includes("@") && password !== ""&& checkbox){
+    submitButton.style.backgroundColor="pink";
+}
+
+})
+
+ 
+     
+  
+
+
+
+
